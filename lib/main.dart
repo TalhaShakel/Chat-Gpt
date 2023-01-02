f5ef6c0da6e3e607a6e0b9cc743ecaf307a28b81
ITEXPLOSERZAHID <itexploserzahid@gmail.com>
2023-01-02 19:33:14 +0500

    Post UI complete

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
         builder: (context, child) {
           return GetMaterialApp(
             builder: EasyLoading.init(),
-            home: MianHomeScreen(),
+            home: SplashScrren(),
             debugShowCheckedModeBanner: false,
           );
         });
